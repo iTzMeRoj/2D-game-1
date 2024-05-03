dc33ce8a217c50b4bb15a9b44b2a87c39fbb5c38
iTzMeRoj <kirafreedom_spartan117@yahoo.com>
2024-05-03 16:38:14 +0800

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: 'Playing League...... ....... ...... jungle diff!',
+    "cs-degree": 'This is my degree in Halo. signed by none other than Capt. CABOOSE.',
+    "sofa-table": 'Sofa table with a bunch of scraps of paper with a note that reads: THEY"RE HER333!!!!.',
+    tv: 'OH GOOD!.. SC2 Tournament is on hosted by lowko: <a href="https://www.youtube.com/@LowkoTV" target="_blank">Lowko</a>',
+    bed: 'The only comfort i need',
+    resume: 'My Resume..... hmmm.. funny i did not know i had one.',
+    projects: 'To do list:1. Learn how to make websites, 2. learnn coding/script, 3. Learn how to make 2d games, Options: Use ChatGPT for help',
+    library: 'A bunch of book i often would not read.',
+    exit: 'exit?... why would you go out?',
+};
